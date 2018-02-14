ff6e0d43d47e13fe1939425952f85d9df1108132
andge86 <andre11241986@gmail.com>
2018-02-14 10:53:10 +0100

    Smal typo corrections

--- acceptanceTesting/src/test/java/steps_definitions/TestRunner.java
+++ acceptanceTesting/src/test/java/steps_definitions/TestRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resourses/features/placeBet.feature",
         glue={"steps_definitions"},
-        plugin = {"pretty", "html:target/cucumber-html-report"}  // generating html report in project folder
+        plugin = {"pretty", "html:target/cucumber-html-report"}  // Generating html report in project folder
 )
 
 public class TestRunner {
